98a9c941f5ec44ee51f4098ccb3cd1fe9ebbf962
Himanshu <rathihimanshu@live.com>
2022-03-02 22:58:42 +0530

    Update listobjectsv2.py

--- s3/listobjectsv2.py
+++ s3/listobjectsv2.py
@@ -6,3 +6,24 @@ response = client.list_objects_v2(
 )
 
 print(response)
+
+
+#need of pagination 
+
+s3_client = boto3.client("s3")
+
+result = s3_client.list_objects(Bucket="my-bucket")
+for obj in result["Contents"]:
+    do_something(obj)
+    
+    
+    
+#pagination 
+
+s3_client = boto3.client("s3")
+
+paginator = s3_client.get_paginator("list_objects")
+pages = paginator.paginate(Bucket="my-bucket")
+for page in pages:
+    for obj in page["Contents"]:
+        do_something(obj)
