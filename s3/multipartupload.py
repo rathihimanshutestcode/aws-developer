d69daa45cdd164841bd3dc24ef9abfbb1397e43e
Himanshu <rathihimanshu@live.com>
2022-03-02 19:24:28 +0530

    Update multipartupload.py

--- s3/multipartupload.py
+++ s3/multipartupload.py
@@ -1,10 +1,27 @@
 import boto3
+import os
+
+s3client = boto3.client('s3')
+
+#print(s3client.list_object_versions(Bucket='marchonebucket-hereawsdev-1'))
+
+
+s3_resource = boto3.resource('s3')
 from boto3.s3.transfer import TransferConfig
+config = TransferConfig(multipart_threshold=1024 * 20,
+                        max_concurrency=10,
+                        multipart_chunksize=1024 * 20,
+                        use_threads=True)
 
-# Set the desired multipart threshold value (5GB)
-GB = 1024 ** 3
-config = TransferConfig(multipart_threshold=5*GB,)
+bucket_name = 'marchonebucket-hereawsdev-2'
+def multipart_upload_boto3():
 
-# Perform the transfer
-s3 = boto3.client('s3')
-s3.upload_file('test.img', 'ohiosamplebucket', 'test.img', Config=config)
+    file_path = '/Users/himanshurathi/Desktop/largepdf.pdf'
+    key = 'largepdf.pdf'
+
+    s3_resource.Object(bucket_name, key).upload_file(file_path,
+                            ExtraArgs={'ContentType': 'text/pdf'},
+                            Config=config
+                            )
+
+print(multipart_upload_boto3())
